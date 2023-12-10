b796b5270672ff66316ce63a2376a988f8aa93e6
kikly0 <145789679+kikly0@users.noreply.github.com>
2023-12-10 15:45:06 +0800

    push commit3

--- src/main/java/halloworld.java
+++ src/main/java/halloworld.java
@@ -4,5 +4,6 @@ public class halloworld {
     public static void main(String[] args) {
         System.out.println("halloworld");
         System.out.println("push_test");
+        System.out.println("ssh_test");
     }
 }
